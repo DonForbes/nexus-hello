48f9d3d02c96bb51c4aa96456cd8a62835c7fbf2
Donald Forbes <donald.forbes@temporal.io>
2024-11-01 15:13:15 +0000

    Initial files.

---
+++ src/main/java/com/donald/nexushello/NexusHelloApplication.java
@@ -0,0 +1,13 @@
+package com.donald.nexushello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusHelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donald/nexushello/NexusHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donald.nexushello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
